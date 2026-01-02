5c6b1a9de74f4f978e2f5aa1f7ba4c2fc4e4d7aa
Pramod <pramod4342@gmail.com>
2026-01-02 15:35:40 +0530

    user model

---
+++ MODELS/user.model.js
@@ -0,0 +1,19 @@
+//url schema defination
+const mongoose = require('mongoose');
+
+const UrlSchema = new mongoose.Schema({
+    shortId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    RedirectUrl :{
+        type : String,
+        required : true
+    },
+    vistHistory : [{timestamp : {type : Number}}]
+},{timestamps : true})
+
+const URL = mongoose.model('url',UrlSchema);
+
+module.exports = URL;
