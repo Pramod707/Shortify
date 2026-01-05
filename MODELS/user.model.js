c2bf9882b5709ac6a26971c4fefa50e5e3bd6a71
Pramod <pramod4342@gmail.com>
2026-01-05 15:38:09 +0530

    userSchema

---
+++ MODELS/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name :{
+        type : String,
+        required : true
+    },
+    email :{
+        type : String,
+        required  : true,
+        unique : true
+    },
+    password :{
+        type : String,
+        required : true
+    }
+},{timeStamps : true});
+const User = mongoose.model('user',UserSchema);
+module.exports = User;
